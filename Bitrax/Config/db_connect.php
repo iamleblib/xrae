98aad9f4eb1cfb70348ca457375198ed13ab358b
iamleblib <chislimjoe12@gmail.com>
2020-05-05 02:56:58 +0100

    Added Database

--- Bitrax/Config/db_connect.php
+++ Bitrax/Config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // check connection "host, username, pswd and dbname"
-$conn = mysqli_connect('localhost', 'root', '', 'bitraxcoin_b');
+$conn = mysqli_connect('localhost', 'u516079930_root', '', 'u516079930_bitraxcoin_b');
 
 if(!$conn){
     echo 'error: ' . mysqli_connect_error();
